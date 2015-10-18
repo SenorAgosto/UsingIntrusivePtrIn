806b4ba064a57586712fc2f3f5921cd4c366d89a
Austin Gilbert <ceretullis@gmail.com>
2015-10-18 18:23:19 -0500

    Boost 1.58, boost::intrusive_ptr relies on macros named using reserved names __[A-Z]*

--- UsingIntrusivePtrIn/platform/intrusive_ptr.hpp
+++ UsingIntrusivePtrIn/platform/intrusive_ptr.hpp
@@ -2,6 +2,12 @@
 
 #pragma warning(push)
 #pragma warning(disable : 4350 4548)
-#include <boost/intrusive_ptr.hpp>
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wreserved-id-macro"
+
+	#include <boost/intrusive_ptr.hpp>
+
+#pragma clang diagnostic pop
 #pragma warning(pop)
 
